b9d0eed70a74c36f2b51751a3ed722e2fb8ee7a3
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2023-01-13 10:10:01 +0530

    new updates

--- src/components/screens/landingPage/Services.jsx
+++ src/components/screens/landingPage/Services.jsx
@@ -15,37 +15,52 @@ function Services({serviceRef}) {
             "id" : 1,
             "image" : Gallery1,
             "title" : "Digital Strategy",
-            "subtitle" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            "subtitle" :"We are a stand-alone brand consultant that collaborates with clients to unlock potential. To generate impact and\
+            desire, we blend incisive strategic thinking with creative design. We combine strategy, design, and technology to produce stunning,\
+            brilliant brands and experiences that expand the brand's future vision"
         },
         {
             "id" : 2,
             "image" : Gallery2,
             "title" : "Branding",
-            "subtitle" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            "subtitle" : "A brand is all about the emotions it evokes in the market. It is not limited to a visual identity, a logo, or a design for a product or\
+            service, but rather, it's a comprehensive system that integrates all channels and points of contact. We provide an end-to-end range of\
+            services, from communications to style manuals, naming, logo creation and placing you right in the market."
         },
         {
             "id" : 3,
             "image" : Gallery3,
-            "title" : "User Experience/User Interface",
-            "subtitle" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            "title" : "Marketing",
+            "subtitle" : "The process of planning and organising the extreme promotional campaigns which will get your business a high-level\
+            recognition & that will lead your business to generate a great revenue & towards an ultimate expansion. Our Marketing Squad at\
+            idenDT will suggest you the most vibrant strategies to implement so that your business will never have a saturation, it keeps growing and\
+            it goes on."
         },
         {
             "id" : 4,
             "image" : Gallery4,
-            "title" : "Development",
-            "subtitle" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            "title" : "Web Development",
+            "subtitle" : "We know that websites speak louder for a brand. Our team of passionate developers and creative designers can help\
+            you create next-level websites by strategically blending user experience and brand storytelling. Our web designers and\
+            developers create responsive websites that feel at home on any device. Product landing pages, marketing sites, or UX/Ul for\
+            company intranet portals-you name it, and we do it all."
         },
         {
             "id" : 5,
             "image" : Gallery5,
-            "title" : "Web Design",
-            "subtitle" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            "title" : "Design and Media",
+            "subtitle" : "The course of creating a Design for your Brand which will let your business get a worldwide recognition & this\
+            design will resemble your brand in the Global Market. Our Crew at idenDT will screen you with the most appropriate Design and will\
+            implement it on the finest Media Platforms where in your desired audience will be aware about who you are."
         },
         {
             "id" : 6,
             "image" : Gallery6,
             "title" : "SEO",
-            "subtitle" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            "subtitle" : "If you are ready to take your business to the next level, we are here to assist you. Using our marketing expertise, we can optimize\
+            elements on your website that affect your ranking. You cannot maintain your top position on Google only by being popular and\
+            visible. It's a fact. Therefore, we focus our off-page SEO service on building credibility for your website. To increase the authority of a\
+            website, we use social media to increase brand awareness and provide quality backlinks."
         },
     ];
     return (
